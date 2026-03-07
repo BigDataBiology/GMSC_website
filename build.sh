929e2945088c2e016c52f86e830fa124eadb06b7
Luis Pedro Coelho <luis@luispedro.org>
2026-03-08 09:14:40 +1000

    BLD Added build.sh to simplify building

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ev
+if test -d dist/ ; then
+    rm -rf dist.backup
+    mv dist dist.backup
+fi
+rm -rf dist/
+mkdir dist
+
+elm make --optimize src/Main.elm --output=dist/index.html
+cp src/style.css dist/
+cp -r src/assets dist/ && cp _redirects di
