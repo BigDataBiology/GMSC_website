52ccc072ec24aa0ef640b36c924986f6e5ef38ce
Luis Pedro Coelho <luis@luispedro.org>
2026-03-08 12:13:44 +1000

    ENH color cluster amino acid groups

--- src/Cluster.elm
+++ src/Cluster.elm
@@ -4,6 +4,7 @@ import Html
 import Html exposing (Html, div, h1, h4, p, span, text)
 import Html.Attributes as HtmlAttr
 import Browser.Navigation as Nav
+import Char
 import Http
 
 import Bootstrap.Table as Table
@@ -204,7 +205,7 @@ viewClusterPage model v =
             [ text "Summary of this 90AA cluster, including consensus sequences, ecological annotation, quality evidence, and linked 100AA members." ]
         , div [ HtmlAttr.class "cluster-summary-grid" ]
             [ viewCard "Consensus sequences"
-                [ viewField "Consensus protein sequence" "cluster-sequence" v.aa
+                [ viewAminoAcidField "Consensus protein sequence" v.aa
                 , viewField "Consensus nucleotide sequence" "cluster-sequence" v.nuc
                 ]
             , viewCard "Annotations"
@@ -232,6 +233,109 @@ viewField label extraClass value =
         [ p [ HtmlAttr.class "cluster-label" ] [ text label ]
         , p [ HtmlAttr.class valueClasses ] [ text value ]
         ]
+
+viewAminoAcidField : String -> String -> Html Msg
+viewAminoAcidField label sequence =
+    div [ HtmlAttr.class "cluster-field" ]
+        [ p [ HtmlAttr.class "cluster-label" ] [ text label ]
+        , div [ HtmlAttr.class "cluster-sequence-view" ]
+            [ div [ HtmlAttr.class "cluster-value cluster-sequence cluster-aa-sequence" ]
+                (List.map viewAminoAcidResidue (String.toList sequence))
+            , p [ HtmlAttr.class "cluster-aa-legend-title" ] [ text "Legend" ]
+            , div [ HtmlAttr.class "cluster-aa-legend" ]
+                (List.map viewAminoAcidLegendItem aminoAcidLegend)
+            ]
+        ]
+
+viewAminoAcidResidue : Char -> Html Msg
+viewAminoAcidResidue residue =
+    span [ HtmlAttr.class ("cluster-aa-residue " ++ aminoAcidGroupClass residue) ]
+        [ text (String.fromChar residue) ]
+
+viewAminoAcidLegendItem : ( String, String, String ) -> Html Msg
+viewAminoAcidLegendItem ( groupLabel, residues, className ) =
+    div [ HtmlAttr.class "cluster-aa-legend-item" ]
+        [ span [ HtmlAttr.class ("cluster-aa-legend-swatch " ++ className) ] [ text "A" ]
+        , span [ HtmlAttr.class "cluster-aa-legend-text" ]
+            [ text (groupLabel ++ " (" ++ residues ++ ")") ]
+        ]
+
+aminoAcidLegend : List ( String, String, String )
+aminoAcidLegend =
+    [ ( "Hydrophobic", "A, V, I, L, M", "aa-hydrophobic" )
+    , ( "Aromatic", "F, W, Y", "aa-aromatic" )
+    , ( "Polar uncharged", "S, T, N, Q, C", "aa-polar" )
+    , ( "Positive", "K, R, H", "aa-basic" )
+    , ( "Negative", "D, E", "aa-acidic" )
+    , ( "Special", "G, P", "aa-special" )
+    , ( "Other / unknown", "X, U, O, *", "aa-other" )
+    ]
+
+aminoAcidGroupClass : Char -> String
+aminoAcidGroupClass residue =
+    case Char.toUpper residue of
+        'A' ->
+            "aa-hydrophobic"
+
+        'V' ->
+            "aa-hydrophobic"
+
+        'I' ->
+            "aa-hydrophobic"
+
+        'L' ->
+            "aa-hydrophobic"
+
+        'M' ->
+            "aa-hydrophobic"
+
+        'F' ->
+            "aa-aromatic"
+
+        'W' ->
+            "aa-aromatic"
+
+        'Y' ->
+            "aa-aromatic"
+
+        'S' ->
+            "aa-polar"
+
+        'T' ->
+            "aa-polar"
+
+        'N' ->
+            "aa-polar"
+
+        'Q' ->
+            "aa-polar"
+
+        'C' ->
+            "aa-polar"
+
+        'K' ->
+            "aa-basic"
+
+        'R' ->
+            "aa-basic"
+
+        'H' ->
+            "aa-basic"
+
+        'D' ->
+            "aa-acidic"
+
+        'E' ->
+            "aa-acidic"
+
+        'G' ->
+            "aa-special"
+
+        'P' ->
+            "aa-special"
+
+        _ ->
+            "aa-other"
 
 viewQualityCard : Cluster -> Bool -> Popover.State -> Html Msg
 viewQualityCard v showDetails pop =
